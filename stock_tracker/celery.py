60003197b2d39bbd4408fb55aa39e6ac596f7c75
saswatpanda001 <saswatkumar787@gmail.com>
2023-08-12 00:20:12 +0530

    third_commit

--- stock_tracker/celery.py
+++ stock_tracker/celery.py
@@ -3,36 +3,40 @@ from __future__ import absolute_import,unicode_literals
 import os 
 from celery import Celery
 import json
-from django.conf import settings
+from django.conf import Settings
+# import django
+# django.setup() 
 
 
 # from celery.schedules import crontab
 # use to schedule task at particular time
-from main.task import update_stock
-from main.models import StockCache
-
-
-
-stocks_data = StockCache.objects.get(User1=1).stocks.all()
-stocks_name = []
-for each in stocks_data:
-    stocks_name.append(each.name)
+# from main.task import update_stock
+# from main.models import StockCache
 
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stock_tracker.settings")
 
+
+
+# stocks_data = StockCache.objects.get(User1=1).stocks.all()
+# stocks_name = []
+# for each in stocks_data:
+#     stocks_name.append(each.name)
+
+
+
 app = Celery("stock_tracker")
 app.conf.enable_utc = False
 app.conf.update(timezone="Asia/Kolkata")
-app.config_from_object(settings,namespace='CELERY')
-app.conf.beat_schedule = {
-    'every-20-seconds':{
-        'task': update_stock,
-        'schedule':20,
-        'args': (stocks_name)
-    }
-}
+app.config_from_object(Settings,namespace='CELERY')
+# app.conf.beat_schedule = {
+#     'every-20-seconds':{
+#         'task': update_stock,
+#         'schedule':20,
+#         'args': (stocks_name)
+#     }
+# }
 
 
 
--- stock_tracker/settings.py
+++ stock_tracker/settings.py
@@ -96,24 +96,24 @@ WSGI_APPLICATION = 'stock_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': "stock_tracker_db",
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#         'USER': 'postgres',
-#         'PASSWORD': '+?adeshsp'
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "stock_tracker_db",
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': '+?adeshsp'
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
